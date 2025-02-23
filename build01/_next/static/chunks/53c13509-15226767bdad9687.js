3ce7ec361ec7b8c0a94996cb9d13abf85d574feb
Ugnius <dzikunskasugnius@gmail.com>
2025-02-23 10:18:34 +0200

    First commit

---
+++ build01/_next/static/chunks/53c13509-15226767bdad9687.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[240],{3872:function(t,a,n){n.d(a,{muy:function(){return c}});var u=n(1810);function c(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"},child:[]}]})(t)}}}]);
